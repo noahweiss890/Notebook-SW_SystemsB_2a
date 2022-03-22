87d66c9678d875f610f589a933a9375e141d1650
noahweiss890 <nweiss890@gmail.com>
2022-03-22 02:29:50 +0200

    added func signatures

---
+++ Notebook.cpp
@@ -0,0 +1,28 @@
+#include "Notebook.hpp"
+#include "Direction.hpp"
+using namespace ariel;
+using namespace std;
+using ariel::Direction;
+
+
+namespace ariel{
+    Notebook::Notebook() {
+
+    }
+
+    void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction direction, string text) {
+
+    }
+
+    string Notebook::read(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length) {
+
+    }
+
+    void Notebook::erase(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length) {
+
+    }
+
+    void Notebook::show(unsigned int page) {
+
+    }
+}
