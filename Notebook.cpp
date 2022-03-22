2f925b9369321c026f35a0b0df96380559869a75
noahweiss890 <nweiss890@gmail.com>
2022-03-22 15:50:54 +0200

    small changes

--- Notebook.cpp
+++ Notebook.cpp
@@ -6,25 +6,27 @@ using ariel::Direction;
 
 
 namespace ariel{
+    const int page_width = 100;
+
     Notebook::Notebook() {
 
     }
 
     void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction direction, string text) {
-        if(column + text.length() >= 100) {
+        if(column + text.length() >= page_width) {
             throw invalid_argument("illegal arguments!");
         }
     }
 
     string Notebook::read(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length) {
-        if(column + length >= 100) {
+        if(column + length >= page_width) {
             throw invalid_argument("illegal arguments!");
         }
         return " ";
     }
 
     void Notebook::erase(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length) {
-        if(column + length >= 100) {
+        if(column + length >= page_width) {
             throw invalid_argument("illegal arguments!");
         }
     }
